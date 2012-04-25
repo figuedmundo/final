4c367b47fcda3099d1d3fb9d3315eb66d2713ca5
Edmundo Figueroa <figuedmundo@gmail.com>
2012-04-24 23:39:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Proyecto Final
+
+  mashup | google maps + decicion de rutas | 
+  montado en una "red social"
+
+  ## Agenda
+
+  ## Itinerario
+
+  ## Fotos
+
+  ## Amigos
+
+  ## Ir a
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FinalU::Application.config.secret_token = 'e4651de988b6cfe3c70216202fd08a7e9d6a6a9f83ef365704bcd810e92ac78072f31ce41ebc794e04688b5953f053774b6abdbd8d61cc17fa4d45d91554fab0'
